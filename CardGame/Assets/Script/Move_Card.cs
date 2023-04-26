b31c8a43889b823b28fd5c68fd558f23daa49716
reome <shub29@naver.com>
2023-04-26 19:53:13 +0900

    card move
    unity card game - card move

--- CardGame/Assets/Script/Move_Card.cs
+++ CardGame/Assets/Script/Move_Card.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Move_Card : MonoBehaviour
 {
     Rigidbody2D myRigid;
-    Vector2 myPosition;
-    Vector2 goalPosition;
-    Vector2 movePosition;
+    Vector3 myPosition;
+    Vector3 goalPosition;
+    Vector3 nextPosition;
 
     public GameObject goal;
+  
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +20,26 @@ public class Move_Card : MonoBehaviour
 
         goalPosition = goal.transform.position;
 
-        movePosition = goalPosition - myPosition;
+        nextPosition = goalPosition - myPosition;
 
         Debug.Log("My Position : " + myPosition);
         Debug.Log("Goal Position : " + goalPosition);
-        Debug.Log("move Position : " + movePosition);
+        Debug.Log("move Position : " + nextPosition);
+        //Debug.Log("단위벡터 : " + nextPosition.normalized);
+        
+        //transform.position = nextPosition + myPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        nextPosition = goalPosition - transform.position;
+        //transform.position = transform.position + goalPosition * Time.deltaTime; //원하는 방향과 다름
+        //천천히 스르륵 가게 하는거
+        //transform.position = transform.position + nextPosition.normalized * Time.deltaTime;
+        //goalPosition - transform.position
+
+        //myRigid.MovePosition(transform.position + nextPosition.normalized * 5 * Time.deltaTime); //안멈춤,,,,?
+        transform.position = Vector3.MoveTowards(transform.position, goalPosition, 5*Time.deltaTime); //정확하게 저 좌표로 이동하게(시작,도착,속도)
     }
 }
