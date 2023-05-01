000219efceba0ded96591cf052ff11fca732b120
reome <shub29@naver.com>
2023-05-01 19:52:24 +0900

    card change
    카드 각각 변경 기능 추가 중

--- CardGame/Assets/Script/GameManager.cs
+++ CardGame/Assets/Script/GameManager.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     //겜매니저가 카드의 spriterenderer 받아서 얘가 지정한 이미지로 변환시켜줌
     //겜매니저 시켜서 카드 모양 바꾸게함
+
     //[SerializeField] private으로 선언 후 inspector 창에서 보고싶을 때 사용, 다른 클래스에서 가져오기는 안됨
     public GameObject[] card = new GameObject[6];
 
@@ -24,12 +27,16 @@ public class GameManager : MonoBehaviour
     bool isMount = false;
     bool isStraight = false;
     
-    int pair = 0;
     int num = 0;
+
+    public Text changeText;
+    public Button[] change = new Button[5];
 
     // Start is called before the first frame update
     void Start()
     {
+        changeText.text = "게임을 시작합니다.";
+
         for (int i = 0; i < randomIdx.Length; i++)
         {
             num = UnityEngine.Random.Range(0, mySprites.Length-1);
@@ -46,16 +53,14 @@ public class GameManager : MonoBehaviour
                 }
             }
         }
-        for (int i = 0; i < card.Length-1; i++)
+        for (int i = 0; i < card.Length - 1; i++)
         {
             cardRenderer[i] = card[i].GetComponent<SpriteRenderer>();
             cardRenderer[i].sprite = mySprites[randomIdx[i]];
             cardList.Add(cardRenderer[i].sprite.name);
         }
-        //카드를 바꾼다면
-        //cardList.Remove(누른카드);
-        //안바꾸면
-        //cardList.RemoveAt(5);
+        
+      
         for (int i = 0; i < cardList.Count; i++)
         {
             onlyNum.Add(Convert.ToInt32(cardList[i].Substring(0, 2)));
@@ -67,22 +72,45 @@ public class GameManager : MonoBehaviour
         onlyNum.Sort();
         onlyShape.Sort();
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void changeCard()
+    {
+        int idx = UnityEngine.Random.Range(0, card.Length-2);
+        cardRenderer[idx].sprite = mySprites[randomIdx[5]];
+       
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Batting()
+    {
+        int pair = 0;
+
         //플러쉬
-        if (onlyShape[0] == onlyShape[onlyShape.Count-1])
+        if (onlyShape[0] == onlyShape[onlyShape.Count - 1])
         {
             isFlush = true;
         }
 
         //마운틴
-        if (onlyNum[0]==06 && onlyNum[1]==10 && onlyNum[2] == 11 && onlyNum[3] == 12 && onlyNum[4] == 13)
+        if (onlyNum[0] == 06 && onlyNum[1] == 10 && onlyNum[2] == 11 && onlyNum[3] == 12 && onlyNum[4] == 13)
         {
             isMount = true;
         }
 
         //스트레이트
-        for (int i = 0; i < onlyNum.Count-1; i++)
+        for (int i = 0; i < onlyNum.Count - 1; i++)
         {
-            if (onlyNum[i] + 1 == onlyNum[i+1])
+            if (onlyNum[i] + 1 == onlyNum[i + 1])
             {
                 isStraight = true;
             }
@@ -94,9 +122,9 @@ public class GameManager : MonoBehaviour
         }
 
         //페어
-        for (int i = 0; i < onlyNum.Count-1; i++)
+        for (int i = 0; i < onlyNum.Count - 1; i++)
         {
-            for (int j = i+1; j < onlyNum.Count; j++)
+            for (int j = i + 1; j < onlyNum.Count; j++)
             {
                 if (onlyNum[i] == onlyNum[j])
                 {
@@ -107,55 +135,52 @@ public class GameManager : MonoBehaviour
 
         if (isFlush && isMount)
         {
-            Debug.Log("로얄 스트레이트 플러쉬");
+            changeText.text = "로얄 스트레이트 플러쉬";
         }
         else if (isFlush && isStraight)
         {
-            Debug.Log("스트레이트 플러쉬");
+            changeText.text = "스트레이트 플러쉬";
         }
         else if (pair == 6)
         {
-            Debug.Log("포카드");
+            changeText.text = "포카드";
         }
         else if (pair == 4)
         {
-            Debug.Log("풀하우스");
+            changeText.text = "풀하우스";
         }
         else if (isFlush)
         {
-            Debug.Log("플러쉬");
+            changeText.text = "플러쉬";
         }
         else if (isMount)
         {
-            Debug.Log("마운틴");
+            changeText.text = "마운틴";
         }
         else if (isStraight)
         {
-            Debug.Log("스트레이트");
+            changeText.text = "스트레이트";
         }
         else if (pair == 3)
         {
-            Debug.Log("트리플");
+            changeText.text = "트리플";
         }
         else if (pair == 2)
         {
-            Debug.Log("투페어");
+            changeText.text = "투페어";
         }
         else if (pair == 1)
         {
-            Debug.Log("원페어");
+            changeText.text = "원페어";
         }
         else
         {
-            Debug.Log("하이카드(족보 완성X)");
+            changeText.text = "하이카드(족보 완성X)";
         }
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //if (change.interactable)
+        //{
+        //    change.interactable= false;
+        //}
     }
-
 }
