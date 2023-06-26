7e3913402f74b176950db58f7c74d43a5d57ec14
Yu chaeyi <shub29@naver.com>
2023-06-26 19:57:19 +0900

    nepenthes 추가
    nepenthes 추가

--- Platformer2D/Assets/02 Scripts/Characters/Character.cs
+++ Platformer2D/Assets/02 Scripts/Characters/Character.cs
@@ -1,10 +1,56 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 //player와 enemy가 character를 상속받게 하려고 추상클래스로 만듦
-public abstract class Character : MonoBehaviour
+public abstract class Character : MonoBehaviour, IHp
 {
     protected Movement movement;
     protected StateMachine stateMachine;
+
+    public float hp
+    {
+        get => _hp;
+        set
+        {
+            if (_hp == value)
+                return;
+
+            float prev = _hp;
+            _hp = value;
+
+            onHpChanged?.Invoke(value);
+            if (prev > value)
+            {
+                onHpDecreased?.Invoke(prev - value);
+                if(value <= _hpMin)
+                {
+                    onHpMin?.Invoke();
+                }
+            }
+            else
+            {
+                onHpIncreased?.Invoke(value - prev);
+                if(value >= _hpMax)
+                {
+                    onHpMax?.Invoke();
+                }
+            }
+        }
+    }
+
+    public float hpMin => _hpMin;
+
+    public float hpMax => _hpMax;
+
+    private float _hp;
+    private float _hpMin;
+    [SerializeField] private float _hpMax;
+
+    public event Action<float> onHpChanged;
+    public event Action<float> onHpDecreased;
+    public event Action<float> onHpIncreased;
+    public event Action onHpMin;
+    public event Action onHpMax;
 
     private void Awake()
     {
@@ -17,5 +63,10 @@ public abstract class Character : MonoBehaviour
             StateType tmpType = value == 0.0f ? StateType.Idle : StateType.Move;
             stateMachine.ChangeState(tmpType);
         };
+    }
+
+    protected virtual void Start()
+    {
+        hp = hpMax;
     }
 }
---
+++ Platformer2D/Assets/02 Scripts/Characters/IHp.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHp
+{
+    float hp { get; set; }
+    float hpMin { get; }
+    float hpMax { get; }
+    event Action<float> onHpChanged;
+    event Action<float> onHpDecreased;
+    event Action<float> onHpIncreased;
+    event Action onHpMin;
+    event Action onHpMax;
+}
