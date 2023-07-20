94a1e6f35e6c9e5496fdb032b61b522edcd6de8e
Yu chaeyi <shub29@naver.com>
2023-07-20 20:40:19 +0900

    datamodel
    datamodel 구현, singleton

---
+++ RPG3D/Assets/02.Scripts/Data/InventoryData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace RPG.Data
+{
+
+    public class InventoryData : IDataModel
+    {
+        public int id { get; set; }
+
+        public struct itemPair
+        {
+            public int id;
+            public int num;
+        }
+
+        public List<itemPair> items;
+
+        public InventoryData(int totalSlot)
+        {
+            items = new List<itemPair>(totalSlot);
+        }
+    }
+
+}
+
